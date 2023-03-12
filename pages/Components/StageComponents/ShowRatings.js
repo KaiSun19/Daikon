d15639f45c6c531f64f2451902a708b93af6e4dd
KaiSun19 <yksun15@gmail.com>
2023-03-12 21:48:54 +0000

    fixed charts

--- pages/Components/StageComponents/ShowRatings.js
+++ pages/Components/StageComponents/ShowRatings.js
@@ -77,6 +77,7 @@ function ShowRatings() {
               {Object.keys(chartData).length > 0 && (
                                 <ScoresBarChart chartData={chartData} />
                 )}
+                
                 <TableContainer component={Paper} sx = {{maxWidth : '580px', backgroundColor: 'white', boxShadow: "none", border: '1px solid #bdbdbd'}}>
                   <Table aria-label="ratings table">
                     <TableHead>
